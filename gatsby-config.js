eb81061fc18a1caf280a6aa3b08ee628571795d2
Snigdha29 <snigdha.twilight@gmail.com>
2021-12-22 09:48:11 +0530

    Customize site

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,27 +1,25 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "Our Gatsby+Wordpress Site",
+    title: `Our Gatsby+Wordpress Site`,
   },
   plugins: [
     {
-      resolve: "gatsby-source-wordpress",
+      resolve: `gatsby-source-wordpress`,
       options: {
-        url: "https://wordpress-658403-2149721.cloudwaysapps.com/graphql",
+        url: `https://wordpress-658403-2149721.cloudwaysapps.com/graphql`,
       },
     },
-    "gatsby-plugin-image",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
+    `gatsby-plugin-image`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: `gatsby-source-filesystem`,
       options: {
-        name: "images",
-        path: "./src/images/",
+        name: 'images',
+        path: './src/images/',
       },
-      __key: "images",
+      __key: 'images',
     },
   ],
 };
